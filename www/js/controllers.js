056f1ed4f91f3d10cdf32f0ba7a53dcec93e2d10
Juan Ignacio Casal <ignaciocasal@gmail.com>
2015-04-24 22:48:39 -0300

    initial commmit

---
+++ www/js/controllers.js
@@ -0,0 +1,11 @@
+angular.module('mappy.controllers', [])
+
+.controller('MapCtrl', function($scope) {})
+.controller('SettingsCtrl', function($scope) {});
+
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   }
+// })
