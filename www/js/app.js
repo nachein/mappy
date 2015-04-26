920f985f0e5c7a876946e4d7ab743b4b4352ff3f
Juan Ignacio Casal <ignaciocasal@gmail.com>
2015-04-26 01:07:59 -0300

    added ngmap (geo. permissions not working)

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('mappy', ['ionic', 'mappy.controllers', 'mappy.services']) 
+angular.module('mappy', ['ionic', 'mappy.controllers', 'mappy.services', 'ngMap']) 
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
